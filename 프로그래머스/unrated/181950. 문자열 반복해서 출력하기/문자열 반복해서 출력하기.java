fa3181c485763db14c11976837b9101141146d0d
heeyoung0101 <100246920+heeyoung0101@users.noreply.github.com>
2023-07-09 23:29:29 +0900

    다른 풀이 추가

--- 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
+++ 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
@@ -11,3 +11,14 @@ public class Solution {
         }
     }
 }
+
+/** 다른 사람의 풀이 
+  - 문자열을 반복하는 메서드 사용 */
+public class Solution2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+        System.out.println(str.repeat(n)); // 문자열(str)을 n번 반복한 것을 출력
+    }
+}
