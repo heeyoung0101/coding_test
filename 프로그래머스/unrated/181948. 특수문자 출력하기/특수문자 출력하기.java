c4c6d7c4bcf4e4e04d398f7dff4452c73094a83a
heeyoung0101 <100246920+heeyoung0101@users.noreply.github.com>
2023-07-09 23:33:16 +0900

    [unrated] Title: 특수문자 출력하기, Time: 256.85 ms, Memory: 58.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        /*
+         * \(백슬래시)와 "(큰 따옴표)와 '(작은 따옴표)는 
+         * 앞에 \(백슬래시)를 붙여야 정상적으로 출력되는 특수문자임
+         */
+        System.out.println("!@#$%^&*(\\\'\"<>?:;");
+    }
+}
