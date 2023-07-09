d2e76d256eb444948109fae9ca64318cffbf2034
heeyoung0101 <100246920+heeyoung0101@users.noreply.github.com>
2023-07-09 22:55:45 +0900

    다른 풀이 추가

--- 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.java
+++ 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.java
@@ -7,3 +7,14 @@ class Solution {
             
     }
 }
+
+/** 다른 사람 풀이 - 나머지를 잘 생각해보자 */
+class Solution2 {
+    public int solution(int num1, int num2) {
+        while (num1 >= num2) { // while문을 돌며 num2의 배수만큼 계속 뺄셈을 하다가 남은 값이 나머지, % 연산자를 쓰지 않고 결과를 냄
+            num1 = num1 - num2;
+        }
+        int answer = num1;
+        return answer;
+    }
+}
