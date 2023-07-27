deaa2a891a436ed634ba89e55012d17f3117fdfb
heeyoung0101 <100246920+heeyoung0101@users.noreply.github.com>
2023-07-28 05:14:05 +0900

    [unrated] Title: 공백으로 구분하기 1, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = {};
+        answer = my_string.split(" "); // 문자열을 공백으로 구분하여 잘라 배열에 넣기
+        return answer;
+    }
+}
