2017deb36564a288e22abfad7c57dfd17ecd95e0
heeyoung0101 <iyou0101@gmail.com>
2023-08-22 06:01:08 +0900

    230822 [세균증식] 다른사람 풀이 추가

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -6,4 +6,11 @@ class Solution {
         }
         return answer;
     }
+
+    /** 다른 사람 풀이 */
+    public int solution2(int n, int t){
+        int answer = 0;
+        answer = n << t; // 이진수 n을 t만큼 쉬프트(2배)
+        return answer;
+    }
 }
