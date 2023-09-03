ef1e978e9d99ac870e0255ddeee8594decc39743
heeyoung0101 <iyou0101@gmail.com>
2023-09-03 11:40:42 +0900

    230903 다른사람풀이 추가

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -41,4 +41,14 @@ class Solution {
         }
         return answer;
     }
+
+    // 다른 사람 풀이 (연산자가 +면 *1, -면 *(-1))
+    public String[] solution2(String[] quiz) {
+        for(int i=0; i<quiz.length; i++){
+            String[] text = quiz[i].split(" ");
+            int result = Integer.parseInt(text[0]) + ( Integer.parseInt(text[2]) * ( text[1].equals("+") ? 1:-1) );
+            quiz[i] = result == Integer.parseInt(text[4])? "O": "X";
+        }
+        return quiz;
+    }
 }
